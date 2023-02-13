b12d03d62867b985f28ce26c4ab34405412ab274
Mateusz Bednarczyk <matib004@gmail.com>
2023-02-13 16:40:30 +0100

    simple sql generated

--- src/pkg/entities/table.go
+++ src/pkg/entities/table.go
@@ -1,7 +1,8 @@
 package entities
 
 type Table struct {
-	Name       string     `json:"Name"`
-	PrimaryKey ColumnType `json:"PrimaryKey"`
-	Columns    []Column   `json:"Columns"`
+	Name        string       `json:"Name"`
+	PrimaryKey  ColumnType   `json:"PrimaryKey"`
+	Columns     []Column     `json:"Columns"`
+	ForeignKeys []ForeignKey `json:"foreign_keys"`
 }
